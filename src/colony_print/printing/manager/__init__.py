23e3d60e7c88a2736a62faec82f1ddf5ad0bf8a3
joamag <joamag@gmail.com>
2014-01-23 13:49:20 +0000

    new init files

--- src/colony_print/printing/manager/__init__.py
+++ src/colony_print/printing/manager/__init__.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import ast
+import exceptions
+import parser
+import system
+import visitor
+
+from ast import *
+from exceptions import *
+from parser import *
+from system import *
+from visitor import *
--- src/colony_print/printing/pdf/__init__.py
+++ src/colony_print/printing/pdf/__init__.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import exceptions
+import system
+import visitor
+
+from exceptions import *
+from system import *
+from visitor import *
