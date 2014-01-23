c6bec4e7c8ad3896f79af820ea4453e07f0a0dba
joamag <joamag@gmail.com>
2014-01-23 16:21:43 +0000

    better js structure

--- src/colony_print/static/example/js/main.js
+++ src/colony_print/static/example/js/main.js
@@ -7,115 +7,31 @@ jQuery(document).ready(function() {
             file.bind("change", function() {
                         var element = jQuery(this);
                         var _file = element[0];
-                        var document = _file.files[0];
+                        var reference = _file.files[0];
 
                         var reader = new FileReader();
                         reader.onload = function(event) {
                             var result = event.target.result;
 
-                            var url = BASE_URL + "print.binie"; //@todo take into account os
+                            var gateway = document.getElementById("colony-gateway");
+                            var format = gateway.pformat();
+
+                            var url = BASE_URL + "print." + format
+                                    + "?base64=1"; //@todo o base 64 tem de ser bem visto e tenho de ver tamanho
                             jQuery.ajax({
                                         url : url,
                                         type : "post",
-                                        data : {
-                                            base64 : "1"
-                                        },
+                                        data : result,
                                         contentType : "text/xml",
                                         success : function(data) {
-                                            console.info(data);
+                                            gateway.print(false, data);
                                         },
                                         error : function() {
-                                            console.info("ERROR")
+                                            alert("Problem with file submission")
                                         }
                                     });
                         };
 
-                        reader.readAsBinaryString(document);
+                        reader.readAsText(reference);
                     });
         });
-
-function version() {
-    try {
-        var gateway = document.getElementById("colony-gateway");
-        alert(gateway.version());
-    } catch (exception) {
-        alert(exception);
-    }
-}
-
-function pformat() {
-    try {
-        var gateway = document.getElementById("colony-gateway");
-        alert(gateway.pformat());
-    } catch (exception) {
-        alert(exception);
-    }
-}
-
-function pdevices() {
-    try {
-        var gateway = document.getElementById("colony-gateway");
-        var devices = gateway.pdevices();
-        var devicesS = "";
-        var isFirst = true;
-        for (var index = 0; index < devices.length; index++) {
-            var device = devices[index];
-            devicesS += isFirst ? "" : "\n"
-            devicesS += device.name + " - " + (device.media || "N/A");
-            isFirst = false;
-        }
-        alert(devicesS);
-    } catch (exception) {
-        alert(exception);
-    }
-}
-
-function foo() {
-    try {
-        var gateway = document.getElementById("colony-gateway");
-        alert(gateway.foo());
-    } catch (exception) {
-        alert(exception);
-    }
-}
-
-function callback() {
-    try {
-        var gateway = document.getElementById("colony-gateway");
-        gateway.callback(function(message) {
-                    alert(message);
-                });
-    } catch (exception) {
-        alert(exception);
-    }
-}
-
-function _alert() {
-    try {
-        var gateway = document.getElementById("colony-gateway");
-        gateway.alert("Hello World :: Olá Mundo");
-    } catch (exception) {
-        alert(exception);
-    }
-}
-
-function _print() {
-    try {
-        var gateway = document.getElementById("colony-gateway");
-        var format = gateway.pformat();
-        switch (format) {
-            case "binie" :
-                gateway.print(true, HELLO_WORD_BINIE_B64);
-                break;
-
-            case "pdf" :
-                gateway.print(true, HELLO_WORD_PDF_B64);
-                break;
-
-            default :
-                break;
-        }
-    } catch (exception) {
-        alert(exception);
-    }
-}
