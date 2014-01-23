da1111cc5f324e589b5f51219633c78a34336dd5
joamag <joamag@gmail.com>
2014-01-23 15:59:03 +0000

    new controllers support

--- src/colony_print/controllers/base.py
+++ src/colony_print/controllers/base.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import cStringIO
+
 import appier
 
 import colony_print
 
+MIME = dict(
+    binie = "text/x-binie",
+    pdf = "application/pdf"
+)
+
 EXAMPLE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\
     <printing_document name=\"hello_world\" font=\"Calibri\" font_size=\"9\">\
         <paragraph text_align=\"center\">\
-            <line><text>01-Lugar da Jóia Porto</text></line>\
+            <line><text>Hello World</text></line>\
         </paragraph>\
     </printing_document>"
 
@@ -19,24 +26,34 @@ class BaseController(appier.Controller):
         appier.Controller.__init__(self, owner, *args, **kwargs)
         self.manager = None
 
-    @appier.route("/print/example", "POST")
-    def print_example(self):
+    @appier.route("/print/example.<format>", "GET")
+    def print_example(self, format):
+        return self.send_print(EXAMPLE, format)
+
+    @appier.route("/print.<format>", "POST")
+    def print_language(self, format):
+        # data = @todo: tenho de sacar os dados de post
+        return self.send_print(EXAMPLE, format)
+
+    def send_print(self, data, format):
+        mime = self.get_mime(format)
         manager = self.get_manager()
 
-        data = EXAMPLE
-        options = dict(name = "pdf")
+        data = data
+        file = cStringIO.StringIO()
+        options = dict(name = format, file = file)
 
-        return manager.print_language(data, options)
+        manager.print_language(data, options)
+        value = file.getvalue()
 
-    @appier.route("/print/win32", "POST")
-    def print_win32(self):
-        return self.template(
-            "account/new.html.tpl",
-            account = {},
-            errors = {}
-        )
+        self.content_type(mime)
+        return value
+
+    def get_mime(self, format):
+        return MIME.get(format, "application/octet-stream")
 
     def get_manager(self):
         if self.manager: return self.manager
         self.manager = colony_print.PrintingManager()
         self.manager.load()
+        return self.manager
