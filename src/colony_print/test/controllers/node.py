ac77dbeff4051af1202049da5791b379f220cde9
joamag <joamag@hive.pt>
2016-09-14 19:17:05 +0100

    new http tests in colony print

---
+++ src/colony_print/test/controllers/node.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+import unittest
+
+import appier
+
+import colony_print
+
+class NodeControllerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app = colony_print.ColonyPrintApp(level = logging.ERROR)
+
+    def tearDown(self):
+        self.app.unload()
+        adapter = appier.get_adapter()
+        adapter.drop_db()
+
+    def test_print_default_o(self):
+        response = self.app.get("/nodes/name/print")
+        self.assertEqual(response.headers["Access-Control-Allow-Origin"].startswith("*"), True)
+        self.assertEqual(response.headers["Access-Control-Allow-Headers"].startswith("*"), True)
